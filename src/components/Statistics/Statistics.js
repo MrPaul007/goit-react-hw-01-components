73175df0869a68aeee4626425703819cb6ce4f6c
Pavlo <75275133+MrPaul007@users.noreply.github.com>
2022-06-22 23:49:48 +0300

    fix some stuff

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -24,7 +24,7 @@ function Statistics({ title, stats }) {
 
 Statistics.propTypes = {
     title: PropTypes.string,
-    items: PropTypes.arrayOf(
+    stats: PropTypes.arrayOf(
       PropTypes.shape({
         id: PropTypes.string.isRequired,
       }),
